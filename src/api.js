49862bc1bb8c6fe443e55a833047e502e4e1e63f
HediHammami <hedihammami95@gmail.com>
2022-08-11 14:07:15 +0100

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0462681ff8mshfaf821b10b0babep1606abjsn91fc34e86e0e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4657628caff218daedf6943668542cc2";
